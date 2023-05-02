ccfe59c6ecd67411e1d7d12b8c290e60cd1a4a07
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-02 12:33:02 +0600

    import chef json data

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const chefData = require("./jsonData/chef.json");
 
 app.use(cors());
 
 app.get("/", (req, res)=>{
     res.send("Flavor Lav server is running");
 })
+
+
+
+
+
+
+
 
 app.listen(port, () =>{
     console.log(`http://localhost:${port}`);
