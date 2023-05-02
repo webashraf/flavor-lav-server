c6439d61a3f4dec1ab30687d32c93354bfdb4d2f
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-02 12:30:42 +0600

    add chef server site and successfully running the server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+app.get("/", (req, res)=>{
+    res.send("Flavor Lav server is running");
+})
+
+app.listen(port, () =>{
+    console.log(`http://localhost:${port}`);
+})
